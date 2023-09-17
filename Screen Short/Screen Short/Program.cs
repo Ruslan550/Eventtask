5fa7a96053b8c09da1b191cd58ca3f30d65d3213
Ruskaa <140341439+Ruslan550@users.noreply.github.com>
2023-09-18 00:07:06 +0400

    Event

---
+++ Screen Short/Screen Short/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+delegate void Func(string str);
+
+class MyClass
+{
+    public void Space(string str)
+    {
+        
+        string result = string.Join("_", str.ToCharArray());
+        Console.WriteLine("Space: " + result);
+    }
+
+    public void Reverse(string str)
+    {
+      
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        string result = new string(charArray);
+        Console.WriteLine("Reverse: " + result);
+    }
+}
+
+
+class Run
+{
+    public void runFunc(Func func, string str)
+    {
+        func(str);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the List :");
+        string str = Console.ReadLine();
+
+        MyClass cls = new MyClass();
+        Run run = new Run();
+
+       
+        run.runFunc(cls.Space, str);
+        run.runFunc(cls.Reverse, str);
+    }
+}
